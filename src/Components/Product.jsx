e098a357a10a3045a23dc4c7c4da136895ba029a
Rohan Singh <rs7289579@gmail.com>
2022-10-10 11:22:23 +0530

    showing color and size in product page

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -36,7 +36,7 @@ const Product = (props) => {
           </div>
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
             <h2 className="text-sm title-font text-gray-500 tracking-widest">
-              {data.category}
+              {data.brand}
             </h2>
             <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
               {data.title}
@@ -100,61 +100,18 @@ const Product = (props) => {
                 </svg>
                 <span className="text-gray-600 ml-3">4 Reviews</span>
               </span>
-              <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                <a className="text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
-                </a>
-                <a className="text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
-                </a>
-                <a className="text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                  </svg>
-                </a>
-              </span>
             </div>
             <p className="leading-relaxed">{data.description}</p>
             <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-              <div className="flex">
-                <span className="mr-3">Color</span>
-                <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                <button className="border-2 border-gray-300 ml-1 bg-purple-500 rounded-full w-6 h-6 focus:outline-none"></button>
-              </div>
-              <div className="flex ml-6 items-center">
-                <span className="mr-3">Size</span>
-                <div className="relative">
+            {data.color?.length>0 && <div className="flex">
+                {data.color && <span className="mr-3">Color</span>}
+                {data.color && data.color.map((item)=>{return <button style={{backgroundColor: item}} className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>})}
+              </div>}
+              {data.size?.length>0 &&<div className="flex ml-6 items-center">
+              {data.size && <span className="mr-3">Size</span>}
+                {data.size && <div className="relative">
                   <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-purple-200 focus:border-purple-500 text-base pl-3 pr-10">
-                    <option>SM</option>
-                    <option>M</option>
-                    <option>L</option>
-                    <option>XL</option>
+                    {data.size.map((item)=>{return <option>{item}</option>})}
                   </select>
                   <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
                     <svg
@@ -169,8 +126,8 @@ const Product = (props) => {
                       <path d="M6 9l6 6 6-6"></path>
                     </svg>
                   </span>
-                </div>
-              </div>
+                </div>}
+              </div>}
             </div>
             <Check_Servicebility />
             <div className="flex flex-wrap justify-between">
